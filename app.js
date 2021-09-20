d2e0302e3c6a9d909edcda72146865bc143e3b32
sergiorodas <sergiorodasbj@hotmail.com>
2021-09-20 01:16:00 -0300

    add intro to express

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('Estoy respondiendo a tu solicitud')
-})
+const express = require('express');
+const app = express();
 
 const port = 3000;
 
-server.listen(port, () => {
-    console.log('escuchando solicitudes')
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.send('Mi respuesta desde express')
+})
+
+app.get('/servicios', (req, res) => {
+    res.send('Estas en la página de servicios')
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+    console.log('Servidor escuchando en el puerto', port)
 })
